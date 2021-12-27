c2b32a2f14f5c0ab6a0c98a72a7e67a89d7919a2
TairHassidVia <tair.hassid@ridewithvia.com>
2021-12-27 15:42:38 +0200

    no message

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,38 @@
+import java.io.File;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Checking");
+        Map<Integer, List<Integer>> edgeIdToPatchIds = new HashMap<>();
+        for (int i = 0 ; i < 10 ; i++) {
+            edgeIdToPatchIds.put(i, new ArrayList<>());
+        }
+
+        int j = 0;
+        for(; j < edgeIdToPatchIds.size() ; j++) {
+            for(int k = 0 ; k < 4 ; k++) {
+                edgeIdToPatchIds.get(j).add(k);
+            }
+        }
     }
+
+    public boolean writeArtifact(Map<Integer, List<Integer>> edgeIdToPatchIds) {
+        Schema schema = new Schema.Parser().parse(SCHEMA_RESOURCE_NAME);
+        File edgeFile = new File(fileName);
+        edgeFile.getParentFile().mkdirs();
+
+        List<GenericRecord> patchIds = new ArrayList<>();
+        for (Map.Entry<Integer, List<Integer>> entry : edgeIdToPatchIds.entrySet()) {
+            GenericRecord m = new GenericData.Record(schema);
+            m.put("edge_id", entry.getKey());
+            m.put("patch_ids", entry.getValue());
+        }
+
+
+        return false;
+    }
+
 }
